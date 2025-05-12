3769713255261d714d467bd54f184e4a1012aeea
ibgdev <ibrahimghorbali605@gmail.com>
2025-05-12 15:44:18 +0100

    joboffer details condidate

--- src/main/java/ibgdev/tunijobs/controllers/CondidateController.java
+++ src/main/java/ibgdev/tunijobs/controllers/CondidateController.java
@@ -1,11 +1,13 @@
 package ibgdev.tunijobs.controllers;
 
+import ibgdev.tunijobs.entity.JobOffer;
 import ibgdev.tunijobs.services.CompanyService;
 import ibgdev.tunijobs.services.JobOfferService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -26,5 +28,16 @@ public class CondidateController {
     public String joboffers(Model model) {
         model.addAttribute("listjoboffers", jobOfferService.findAll() );
         return "condidate/joboffers";
+    }
+
+    @GetMapping("/joboffers/details/{id}")
+    public String showJobOfferDetails(@PathVariable("id") Long id, Model model) {
+        JobOffer jobOffer = jobOfferService.findJobOfferById(id);
+        if (jobOffer == null) {
+            return "redirect:/condidate/joboffers?error=notfound"; //notfound
+        }
+        model.addAttribute("id", id);
+        model.addAttribute("offer", jobOffer);
+        return "condidate/joboffer-details"; // This is the name of your Thymeleaf template
     }
 }

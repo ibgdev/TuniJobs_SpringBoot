1e99eb21950cb0154e49641a6275d3adc2f02457
ibgdev <ibrahimghorbali605@gmail.com>
2025-05-16 14:24:27 +0100

    final version

--- src/main/java/ibgdev/tunijobs/controllers/AdminController.java
+++ src/main/java/ibgdev/tunijobs/controllers/AdminController.java
@@ -1,12 +1,37 @@
 package ibgdev.tunijobs.controllers;
 
+import ibgdev.tunijobs.services.CategoryService;
+import ibgdev.tunijobs.services.CompanyService;
+import ibgdev.tunijobs.services.JobOfferService;
+import ibgdev.tunijobs.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class AdminController {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private JobOfferService jobOfferService;
+    @Autowired
+    private CompanyService companyService;
+    @Autowired
+    private CategoryService categoryService;
+
     @GetMapping("/admin")
-    public String AdminDashboard() {
+    public String AdminDashboard(Model model) {
+        int nbUser = userService.findAll().size();
+        int nbJobs = jobOfferService.findAll().size();
+        int nbCompanies = companyService.findAll().size();
+        int nbCategories = categoryService.findAll().size();
+
+
+        model.addAttribute("nbUser", nbUser);
+        model.addAttribute("nbJobs", nbJobs);
+        model.addAttribute("nbCompanies", nbCompanies);
+        model.addAttribute("nbCategories", nbCategories);
         return "dashboard";
     }
 }
